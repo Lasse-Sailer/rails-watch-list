2464c0c3a966d7d6e908a51ffca1586bb4267d34
Lasse Sailer <l.sailer@online.de>
2022-11-25 16:26:35 +0100

    bookmarks controller fixed

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,7 +7,7 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    @bookmark = Bookmark.new
+    @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
